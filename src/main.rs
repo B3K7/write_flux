f91f34ee989d60ed0e815a95c9b5fb06d9b5da32
B3K7 <79159661+B3K7@users.noreply.github.com>
2023-02-28 11:18:59 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-/* write_flux/src/main.rs */
+/* write_flux/src/main/rs. */
 
 use clap::Parser;
 use futures::executor::block_on;
 use reqwest::Error;
 use std::fs;
 use serde_derive::{Deserialize, Serialize};
+use chrono::DateTime;
 
 /// Send point measurement(s) to influxdb2 target
 #[derive(Parser, Debug)]
@@ -30,6 +31,7 @@ struct FlxStruct {
 struct DataStruct {
     tag    : String,
     measure: i64,
+    datetime : Option<String>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -41,12 +43,13 @@ struct MeasureStruct {
 }
 
 async fn wr_nflx_msg( target_path : &str, measurement_path : &str ) -> Result<(), Box<dyn std::error::Error>> {
+    // send message
 
     use futures::prelude::*;
     use influxdb2::models::DataPoint;
     use influxdb2::Client;
 
-    //target endpoint
+    //ingest target endpoint
     let endpoint = {
         let endpoint = fs::read_to_string(target_path)
             .expect("Unable to read file");
@@ -55,6 +58,8 @@ async fn wr_nflx_msg( target_path : &str, measurement_path : &str ) -> Result<()
 
     let client = Client::new(&endpoint.url, &endpoint.org, &endpoint.token);
 
+    // ingest measurements
+    // todo limit size main memory
     let measurement = {
         let measurement = fs::read_to_string(measurement_path)
             .expect("Unable to read file");
@@ -63,16 +68,32 @@ async fn wr_nflx_msg( target_path : &str, measurement_path : &str ) -> Result<()
 
     let mut points = Vec::new();
 
+    // marshall message
     for iter in &measurement._records {
-        let point : DataPoint =
-            DataPoint::builder(&measurement.topic.clone())
-                .tag(  measurement.tagunits.clone(), iter.tag.clone())
-                .field(measurement.units.clone(),    iter.measure)
-                .build()
-                .unwrap();
-        points.push(point.to_owned());
+        if iter.datetime.is_none() {
+            let point : DataPoint =
+                DataPoint::builder(&measurement.topic.clone())
+                    .tag(  measurement.tagunits.clone(), iter.tag.clone())
+                    .field(measurement.units.clone(),    iter.measure)
+                    .build()
+                    .unwrap();
+            points.push(point.to_owned());
+        } else {
+            let dt =  DateTime::parse_from_rfc3339(iter.datetime.as_ref().unwrap()).unwrap();
+
+            let point : DataPoint =
+                DataPoint::builder(&measurement.topic.clone())
+                    .tag(  measurement.tagunits.clone(), iter.tag.clone())
+                    .field(measurement.units.clone(),    iter.measure)
+                    .timestamp(dt.timestamp())
+                    .build()
+                    .unwrap();
+            points.push(point.to_owned());
+
+        }
     }
 
+    //send message
     Ok(client.write(&endpoint.bucket, stream::iter(points)).await?)
 }
 
@@ -84,6 +105,7 @@ async fn main() -> Result<(), Error>  {
     let target_path = args.target_json;
     let measurement_path = args.measurement_json;
 
+    //write message
     let my_result = block_on(wr_nflx_msg(&target_path, &measurement_path));
 
     my_result.unwrap();
